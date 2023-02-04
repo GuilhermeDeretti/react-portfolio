ab5a12a690bb8a854f1f846706436eb3b5d0755f
Guitti <guilhermederetti9@gmail.com>
2023-02-04 15:22:01 +0000

    change bootstrap to version 4/ change gh-pages to dev-dependencies/ add starter for Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Blog from './components/pages/Blog';
+import Contact from './components/pages/Contact';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello World</p>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        {/* Wrap Route elements in a Routes component */}
+        <Routes>
+          {/* Define routes using the Route component to render different page components at different paths */}
+          {/* Define a default route that will render the Home component */}
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+          {/* Define a route that will have descendant routes */}
+          <Route path="contact/*" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
