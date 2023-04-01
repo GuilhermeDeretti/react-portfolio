ce1918cdf42c0fdc002fd90c81e5e5c7875f2dbe
Guitti <guilhermederetti9@gmail.com>
2023-04-01 12:42:01 +0100

    added hazard stripe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Stripe from './components/Stripe';
 import Home from './components/pages/Home';
 import ProjectGallery from './components/pages/ProjectGallery';
 import Contact from './components/pages/Contact';
@@ -15,6 +16,7 @@ function App() {
           {/* Define a route that will have descendant routes */}
           <Route path="react-portfolio/contact" element={<Contact />} />
         </Routes>
+        <Stripe />
       </div>
     </Router>
   );
